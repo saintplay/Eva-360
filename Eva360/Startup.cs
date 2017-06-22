0869c8b1ae724c043e8aeaf0f7fb6eb73fca4d3e
Francis Marquez <fran.mp07@gmail.com>
2017-06-22 15:07:06 -0500

    Hello world

---
+++ Eva360/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eva360.Startup))]
+namespace Eva360
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
