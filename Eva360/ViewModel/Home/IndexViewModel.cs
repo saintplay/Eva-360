471b53201063628123205858992f92d44fe167ef
unknown <LAB_SI_B_404_PC17@PCSIB404-17.upc.edu.pe>
2017-06-26 14:54:36 -0500

    Hola Vue, MUERE Jquery

--- Eva360/ViewModel/Home/IndexViewModel.cs
+++ Eva360/ViewModel/Home/IndexViewModel.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Eva360.Models;
+using static Eva360.Controllers.HomeController;
 
 namespace Eva360.ViewModel.Home
 {
     public class IndexViewModel
     {
-        public List<Usuario> LstUsuario { get; set; }
+        public List<CustomUser> LstUsuario { get; set; }
     }
 }
