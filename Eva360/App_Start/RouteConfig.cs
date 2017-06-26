acfa81a8c8db76948587193e0d3154f74fd41813
Diego Jara <saintplay96@gmail.com>
2017-06-26 00:57:48 -0500

    Aisla nombre del servidor

--- Eva360/App_Start/RouteConfig.cs
+++ Eva360/App_Start/RouteConfig.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -11,6 +13,12 @@ namespace Eva360
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var settings = ConfigurationManager.ConnectionStrings["EVA360Entities"];
+            var fi = typeof(ConfigurationElement).GetField("_bReadOnly", BindingFlags.Instance | BindingFlags.NonPublic);
+            fi.SetValue(settings, false);
+            String expanded = Environment.ExpandEnvironmentVariables(settings.ConnectionString);
+            settings.ConnectionString = expanded;
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
