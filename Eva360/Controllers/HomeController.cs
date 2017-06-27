88d6812833469995728e20d10637f7dcc46329c3
Diego Jara <saintplay96@gmail.com>
2017-06-27 01:01:35 -0500

    Agrega rightbar

--- Eva360/Controllers/HomeController.cs
+++ Eva360/Controllers/HomeController.cs
@@ -10,28 +10,11 @@ namespace Eva360.Controllers
 {
     public class HomeController : BaseController
     {
-        public class CustomUser {
-            public string Nombre { get; set; }
-            public string Apellido { get; set; }
-            public string Email { get; set; }
-
-            public CustomUser(string Nombre, string Apellido, string Email) {
-                this.Nombre = Nombre;
-                this.Apellido= Apellido;
-                this.Email = Email;
-            }
-        }
-
         public ActionResult Index()
         {
             var context = new EVA360Entities();
             var viewModel = new IndexViewModel();
-            viewModel.LstUsuario = context.Usuario.AsEnumerable().Select(x => new CustomUser(
-                    x.Nombre,
-                    x.Apellido,
-                    x.Email
-                )
-            ).ToList();
+            viewModel.LstUsuario = context.Usuario.ToList();
       
             return View(viewModel);
         }
