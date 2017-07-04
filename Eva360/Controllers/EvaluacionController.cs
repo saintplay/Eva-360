2e513c1702f9ba2cc53a65a7d71aac8183a5b680
Drewmsu <fran.mp07@gmail.com>
2017-07-04 18:13:56 -0500

    Listar Evaluaciones (sin vista)

---
+++ Eva360/Controllers/EvaluacionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Eva360.Models;
+
+namespace Eva360.Controllers
+{
+    public class EvaluacionController : Controller
+    {
+        [NonAction]
+        public JsonResult getData()
+        {
+            var context = new EVA360Entities();
+            context.Configuration.LazyLoadingEnabled = false;
+            context.Configuration.ProxyCreationEnabled = false;
+
+            var LstEvaluaciones = context.Evaluacion.ToList();
+
+            return Json(new { evaluaciones = LstEvaluaciones }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ListarEvaluaciones()
+        {
+            return getData();
+        }
+    }
+}
