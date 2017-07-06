a21270b38c56d7c6be3895c4b6a1a31e20d13596
Drewmsu <fran.mp07@gmail.com>
2017-07-06 18:13:58 -0500

    Implementa Login (Falta definir roles)

---
+++ Eva360/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Eva360.ViewModel.Login;
+using Eva360.Models;
+using Eva360.Helpers;
+
+namespace Eva360.Controllers
+{
+    public class LoginController : BaseController
+    {
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Login()
+        {
+            var viewModel = new LoginForm();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginForm loginModel)
+        {
+            if (!ModelState.IsValid) {
+                PostMessage(MessageType.Warning, "Debe completar todos los campos");
+                return View(loginModel);    
+            }
+
+            var context = new EVA360Entities();
+            var usuario = context.Usuario
+                          .FirstOrDefault(u => u.Codigo == loginModel.Usuario &&
+                            u.Password == (PasswordHelper.MD5Hash(loginModel.Password) + u.Salt));
+
+            if (usuario != null) {
+
+                //Login exitoso
+                PostMessage(MessageType.Success, "Bienvenido" + usuario.Nombre);
+
+                Session["UsuarioId"] = usuario.UsuarioId;
+                Session["Nombre"] = usuario.Nombre;
+                Session["NombreCompleto"] = usuario.Apellido + usuario.Nombre;
+            }
+            else {
+                PostMessage(MessageType.Error, "Usuario/Contraseña incorrectos");
+            }
+
+            return View(loginModel);
+        }
+
+      
+    }
+}
---
+++ Eva360/ViewModel/Login/LoginForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Eva360.Models;
+
+namespace Eva360.ViewModel.Login
+{
+    public class LoginForm
+    {
+        [Required]
+        public String Usuario { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+    }
+}
